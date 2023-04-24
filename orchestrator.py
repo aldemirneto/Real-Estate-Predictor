879a7e2c2242bcd976a17bbf46bb462ea140aff5
Aldemir Humberto Soares Neto <56364675+aldemirneto@users.noreply.github.com>
2023-04-23 21:44:48 -0300

    implementando melhor tratamento de duplicata

--- orchestrator.py
+++ orchestrator.py
@@ -19,13 +19,19 @@ with open(filename, 'r', encoding='utf-8') as csv_file:
     reader = csv.reader(csv_file, delimiter=';')
     header = next(reader)
 
+    # Find the index of the "Data_scrape" column
+    date_col_index = header.index('Data_scrape')
+
     # Create a set to store the unique rows
     unique_rows = set()
 
     # Iterate over the rows in the CSV file
     for row in reader:
+        # Remove the "Data_scrape" column from the row
+        row_without_date = row[:date_col_index] + row[date_col_index + 1:]
+
         # Convert the row to a tuple and add it to the set
-        unique_rows.add(tuple(row))
+        unique_rows.add(tuple(row_without_date))
 
 # Write the header and unique rows back to the CSV file
 with open(filename, 'w', newline='', encoding='utf-8') as csv_file:

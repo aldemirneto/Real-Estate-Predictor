0f3e92e21cfd127e525eff5fc36689c59ea7c578
Aldemir Humberto Soares Neto <56364675+aldemirneto@users.noreply.github.com>
2023-05-08 11:13:37 -0300

    Update orchestrator_scrape.py

--- orchestrator_scrape.py
+++ orchestrator_scrape.py
@@ -10,26 +10,26 @@ except:
 print('Iniciando scrape de imobiliárias')
 try:
     print('Inicio de scrape Junqueira')
-    imobiliárias.junqueira.run()
+#    imobiliárias.junqueira.run()
     print('Fim de scrape Junqueira')
 except Exception as e:
     print('junqueira', e)
 try:
     print('Inicio de scrape Frias Neto')
-    imobiliárias.frias_neto.run()
+ #   imobiliárias.frias_neto.run()
     print('Fim de scrape Frias Neto')
 except Exception as e:
     print('frias_neto', e)
 try:
     print('Inicio de scrape São Judas')
-    imobiliárias.sao_judas.run()
+  #  imobiliárias.sao_judas.run()
     print('Fim de scrape São Judas')
 except Exception as e:
     print('sao_judas', e)
 
 try:
     print('Inicio de scrape duo imoveis')
-    imobiliárias.duo.run()
+   # imobiliárias.duo.run()
     print('Fim de scrape duo imoveis')
 except Exception as e:
     print('duo', e)

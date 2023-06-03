64448a35546ad402be5c52336bd996fa986f5240
aldemir.neto <humbertoaldemir@gmail.com>
2023-06-03 19:05:10 -0300

    fix: resolvendo problema de deploy

--- pages/pagina_3.py
+++ pages/pagina_3.py
@@ -7,7 +7,7 @@ from xgboost import XGBRegressor
 
 
 # Function to load the XGBoost model
-@st.cache_resource(allow_output_mutation=True)
+@st.cache_resource()
 def load_model():
     xgb_model = XGBRegressor()
     xgb_model.load_model(st.secrets['model']['MODEL'])

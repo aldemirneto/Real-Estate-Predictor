a87e7a994a183b50ea621b6ef6ca7bfeea8faec8
aldemir.neto <humbertoaldemir@gmail.com>
2023-07-04 00:26:50 -0300

    feat: alterando tratamento

--- Tratamento/Duplicatas.py
+++ Tratamento/Duplicatas.py
@@ -37,13 +37,19 @@ def validate_and_filter_data(df: pd.DataFrame) -> pd.DataFrame:
     valid_rows = [validate_data(row) for _, row in df.iterrows()]
     valid_rows = [row for row in valid_rows if row is not None]  # filter out None values
     valid_df = pd.DataFrame([row.dict() for row in valid_rows])
-    valid_df = valid_df.drop_duplicates(subset=['preco', 'area', 'quartos', 'vagas', 'banheiros', 'link', 'Imobiliaria','bairro'])
-    valid_df.loc[valid_df.duplicated(
+    merged_data = valid_df.copy()
+    merged_data.loc[merged_data.duplicated(
         subset=['preco', 'area', 'quartos', 'vagas', 'banheiros', 'link', 'Imobiliaria', 'bairro'],
         keep=False), 'last_seen'] = date.today().strftime('%Y-%m-%d')
-    valid_df = valid_df[valid_df['area'] > 2]
-    valid_df.fillna({'vagas': 0, 'banheiros': 0, 'quartos': 0}, inplace=True)
-    return valid_df
+    # update the existing data with the new data with the link column as the key
+
+    merged_data = merged_data.drop_duplicates(
+        subset=['preco', 'area', 'quartos', 'vagas', 'banheiros', 'link', 'Imobiliaria', 'bairro'])
+
+
+    merged_data = merged_data[merged_data['area'] > 2]
+    merged_data.fillna({'vagas': 0, 'banheiros': 0, 'quartos': 0}, inplace=True)
+    return merged_data
 
 def run(filename: str) -> None:
     existing_data = pd.read_csv(filename, delimiter=';')

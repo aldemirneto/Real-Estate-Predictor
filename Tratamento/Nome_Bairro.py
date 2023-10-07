0747e0ae067a70062de5a77e8c19e772a0539000
aldemir.neto <humbertoaldemir@gmail.com>
2023-10-07 20:34:15 -0300

    feat: implementando aluguel

--- Tratamento/Nome_Bairro.py
+++ Tratamento/Nome_Bairro.py
@@ -7,7 +7,7 @@ def replace_chars(string):
     # Replace accented characters with their unaccented equivalents
     string = unicodedata.normalize('NFKD', string).encode('ASCII', 'ignore').decode('utf-8')
     # Replace spaces with underscores
-    string = string.replace(', piracicaba', '').replace(', PIRACICABA', '')
+    string = string.replace(', piracicaba', '').replace(', PIRACICABA', '').strip()
     string = string.replace(' ', '_')
     string = string.capitalize()
     return string
